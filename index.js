1ea87bafa11c5ea9a1aea0b9db7a1216f263bdeb
Pedro Fuentes <pedro@groupon.com>
2016-05-26 19:33:05 -0700

    add support for submenus

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 var objectAssign = require('object-assign');
 var sep = {};
 
-function create(input, opts) {
+function create(input, opts, menuLevel) {
+	menuLevel = menuLevel || 0;
+
 	return input.map(function (line) {
+		var submenuText = '';
 		if (typeof line === 'string' || typeof line === 'number') {
 			line = {text: line};
 		}
@@ -21,15 +24,22 @@ function create(input, opts) {
 			throw new Error('text required');
 		}
 
+		if (typeof line.submenus === 'object' && line.submenus.length > 0) {
+			submenuText = '\n' + create(line.submenus, opts, menuLevel + 1);
+			delete line.submenus;
+		}
+
+		var prefix = '--'.repeat(menuLevel);
+
 		return text.split('\n').map(function (textLine) {
-			return textLine + '|' + Object.keys(line).map(function (x) {
+			return prefix + textLine + '|' + Object.keys(line).map(function (x) {
 				if (x === 'href') {
 					x = encodeURI(x);
 				}
 
 				return x + '="' + line[x] + '"';
 			}).join(' ');
-		}).join('\n');
+		}).join('\n').concat(submenuText);
 	}).join('\n');
 }
 
