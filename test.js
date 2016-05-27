ba0b23db375c6ae68289b12ac921ecbf0ca3065f
Pedro Fuentes <pedro@groupon.com>
2016-05-26 19:32:08 -0700

    add tests for expected input and output

--- test.js
+++ test.js
@@ -11,7 +11,20 @@ test(t => {
 		{
 			text: 'Unicorns',
 			color: '#ff79d7',
-			href: 'https://www.youtube.com/watch?v=9auOCbH5Ns4'
+			href: 'https://www.youtube.com/watch?v=9auOCbH5Ns4',
+			submenus: [
+				{
+					text: '1st Level Submenu - A',
+					submenus: [
+						{
+							text: '2nd level Submenu'
+						}
+					]
+				},
+				{
+					text: '1st Level Submenu - B'
+				}
+			]
 		},
 		m.sep,
 		'Ponies'
@@ -20,6 +33,9 @@ test(t => {
 	const expected = `
 ❤|color="red" dropdown="false"
 Unicorns|color="#ff79d7" href="https://www.youtube.com/watch?v=9auOCbH5Ns4"
+--1st Level Submenu - A|
+----2nd level Submenu|
+--1st Level Submenu - B|
 ---
 Ponies|
 `.trim();
