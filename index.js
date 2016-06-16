b103a4b91bb71dd7425775bde59d157cb81f5edd
Pedro Fuentes <pedro@groupon.com>
2016-06-16 13:49:29 -0700

    addressing style comments

--- index.js
+++ index.js
@@ -1,43 +1,40 @@
 'use strict';
 const sep = {};
+const create = (input, opts, menuLevel = 0) => input.map(line => {
+	let submenuText = '';
+	if (typeof line === 'string' || typeof line === 'number') {
+		line = {text: line};
+	}
 
-const create = (input, opts, menuLevel = 0) => {
-	return input.map(line => {
-		let submenuText = '';
-		if (typeof line === 'string' || typeof line === 'number') {
-			line = {text: line};
-		}
+	if (line === sep) {
+		return '---';
+	}
 
-		if (line === sep) {
-			return '---';
-		}
+	line = Object.assign(line, opts);
 
-		line = Object.assign(line, opts);
+	const text = String(line.text);
+	delete line.text;
 
-		const text = String(line.text);
-		delete line.text;
+	if (!text) {
+		throw new Error('text required');
+	}
 
-		if (!text) {
-			throw new Error('text required');
-		}
+	if (typeof line.submenu === 'object' && line.submenu.length > 0) {
+		submenuText = `\n${create(line.submenu, opts, menuLevel + 1)}`;
+		delete line.submenu;
+	}
 
-		if (typeof line.submenu === 'object' && line.submenu.length > 0) {
-			submenuText = `\n${create(line.submenu, opts, menuLevel + 1)}`;
-			delete line.submenu;
-		}
+	const prefix = '--'.repeat(menuLevel);
 
-		const prefix = '--'.repeat(menuLevel);
+	return text.split('\n').map(textLine => {
+		const options = Object.keys(line).map(key => {
+			const value = key === 'href' ? encodeURI(line[key]) : line[key];
+			return `${key}="${value}"`;
+		}).join(' ');
 
-		return text.split('\n').map(textLine => {
-			const options = Object.keys(line).map(key => {
-				const value = key === 'href' ? encodeURI(line[key]) : line[key];
-				return `${key}="${value}"`;
-			}).join(' ');
-
-			return `${prefix}${textLine}|${options}`;
-		}).join('\n').concat(submenuText);
-	}).join('\n');
-};
+		return `${prefix}${textLine}|${options}`;
+	}).join('\n').concat(submenuText);
+}).join('\n');
 
 module.exports = (input, opts) => {
 	console.log(create(input, opts));
