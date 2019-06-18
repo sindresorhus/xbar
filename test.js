17d6545a4d99dfa788bec053f0e0e747a598a893
sasikanth513 <dronavalli.sasi@gmail.com>
2019-06-18 23:51:45 +0530

    correctly encodes special characters in the `href` option

--- test.js
+++ test.js
@@ -62,3 +62,22 @@ test('`text` property validation', t => {
 		bitbar([{text: 1}]);
 	}, errorMessage);
 });
+
+test('correctly encodes special characters in the `href` option', t => {
+	const actual = bitbar.create([
+		{
+			text: 'Single Quote',
+			href: 'https://www.youtube.com/watch?v=\'9auOCbH5Ns4'
+		},
+		{
+			text: 'Ampercent',
+			href: 'https://www.youtube.com/watch?v=&9auOCbH5Ns4'
+		}
+	]);
+	const expected = `
+Single Quote|href="https://www.youtube.com/watch?v=%279auOCbH5Ns4"
+Ampercent|href="https://www.youtube.com/watch?v=%269auOCbH5Ns4"
+`.trim();
+
+	t.is(actual, expected);
+});
