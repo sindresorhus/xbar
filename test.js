bffe0e3718dd5b03a452109a2e8a2b78a00f45ac
sasikanth513 <dronavalli.sasi@gmail.com>
2019-06-20 08:12:25 +0530

    add double quotes test

--- test.js
+++ test.js
@@ -70,12 +70,17 @@ test('correctly encodes special characters in the `href` option', t => {
 			href: 'https://www.youtube.com/watch?v=\'9auOCbH5Ns4'
 		},
 		{
+			text: 'Double Quotes',
+			href: 'https://www.youtube.com/watch?v="9auOCbH5Ns4"'
+		},
+		{
 			text: 'Ampercent',
 			href: 'https://www.youtube.com/watch?v=&9auOCbH5Ns4'
 		}
 	]);
 	const expected = `
 Single Quote|href="https://www.youtube.com/watch?v=%279auOCbH5Ns4"
+Double Quotes|href="https://www.youtube.com/watch?v=%229auOCbH5Ns4%22"
 Ampercent|href="https://www.youtube.com/watch?v=%269auOCbH5Ns4"
 `.trim();
 
