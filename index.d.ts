6ab63f08d5bca8e746252de467ad8229637948e1
Gary King <garyking@gmail.com>
2019-10-20 14:12:25 -0400

    Add comments for each property.

--- index.d.ts
+++ index.d.ts
@@ -2,36 +2,103 @@ declare namespace bitbar {
 	// Options for the original BitBar program, taken from here:
 	// https://github.com/matryer/bitbar#plugin-api
 	interface BitbarOptions {
-		// The only required property.
+		/**
+		 * The text to show. The only required property.
+		 */
 		text: string;
 
+		/**
+		 * Make the item clickable.
+		 */
 		href?: string;
+
+		/**
+		 * Change the text color.
+		 */
 		color?: string;
+
+		/**
+		 * Change the text font.
+		 */
 		font?: string;
+
+		/**
+		 * Change the text size.
+		 */
 		size?: number;
+
+		/**
+		 * Run a script.
+		 */
 		bash?: string;
 
-		// There are up to 5 params hardcoded into the program.
+		/**
+		 * Params to specify as arguments for the script, specified in `bash`. There
+		 * are up to 5 params available in BitBar.
+		 */
 		param1?: string;
 		param2?: string;
 		param3?: string;
 		param4?: string;
 		param5?: string;
 
+		/**
+		 * Start the script with Terminal.
+		 */
 		terminal?: boolean;
+
+		/**
+		 * Refresh the plugin.
+		 */
 		refresh?: boolean;
+
+		/**
+		 * Show the item in the dropdown.
+		 */
 		dropdown?: boolean;
+
+		/**
+		 * Truncate the line to the specified number of characters.
+		 */
 		length?: number;
+
+		/**
+		 * Trim the leading and trailing whitespace from the text.
+		 */
 		trim?: boolean;
+
+		/**
+		 * Mark the line as an alternate to the previous line, for when the Option
+		 * key is pressed, in the dropdown.
+		 */
 		alternate?: boolean;
+
+		/**
+		 * Set an image for the item. It must be a base64 encoded string.
+		 */
 		templateImage?: string;
+
+		/**
+		 * Set an image for this item. It must be a base64 encoded string.
+		 */
 		image?: string;
+
+		/**
+		 * Convert text to emojis, such as :mushroom:.
+		 */
 		emojize?: boolean;
+
+		/**
+		 * Enable parsing of ANSI codes.
+		 */
 		ansi?: boolean;
 	}
 
 	// Options provided by this module.
 	interface Options extends BitbarOptions {
+		/**
+		 * Add a submenu to the item. A submenu is composed of an array of items.
+		 */
 		submenu?: (string | BitbarOptions)[];
 	}
 }
@@ -39,8 +106,14 @@ declare namespace bitbar {
 declare const bitbar: {
 	(items: (string | bitbar.Options)[], options?: bitbar.BitbarOptions): void;
 
+	/**
+	 * Add a separator.
+	 */
 	separator: symbol;
 
+	/**
+	 * Check that dark mode is enabled.
+	 */
 	darkMode: boolean;
 };
 
