3f8b281ebada58ec0ce9ca7cb7c9a80883191a50
garyking <garyking@gmail.com>
2019-11-19 01:27:46 -0500

    Accept top-level separator in the TypeScript types (#21)

--- index.test-d.ts
+++ index.test-d.ts
@@ -8,7 +8,24 @@ expectType<void>(
 			color: bitbar.darkMode ? 'white' : 'red',
 			dropdown: false
 		},
-	]),
+		bitbar.separator,
+		{
+			text: 'Unicorns',
+			color: '#ff79d7',
+			submenu: [
+				{
+					text: ':tv: Video',
+					href: 'https://www.youtube.com/watch?v=9auOCbH5Ns4'
+				},
+				{
+					text: ':book: Wiki',
+					href: 'https://en.wikipedia.org/wiki/Unicorn'
+				}
+			]
+		},
+		bitbar.separator,
+		'Ponies'
+	])
 );
 
 expectType<boolean>(bitbar.darkMode);
